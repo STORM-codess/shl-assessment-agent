6eb6943763cdf69c142bf0abbf6275318934761e
Aryan Srivastava <aryanshrivastava2411@gmail.com>
2026-05-17 20:22:22 +0530

    retrieval.py update

--- app/services/retrieval.py
+++ app/services/retrieval.py
@@ -10,7 +10,7 @@ import json
 def load_catalog():
 
     with open(
-        "app/data/processed_catalog.json",
+        "data/processed_catalog.json",
         "r",
         encoding="utf-8"
     ) as file:
